976183694aea60729d636fc3273c391812e06f2c
akshayksuresh3 <akshay.k@businessonbot.com>
2022-03-04 23:05:06 +0530

    clear timer

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,7 +12,11 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6)
+    const handler = setTimeout(() => setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6), 500)
+    return () => {
+      console.log('clean up')
+      clearTimeout(handler);
+    };
   }, [enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
